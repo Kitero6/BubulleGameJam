7e083b1529514c3174e11e22be74c09990c67f0e
Bastien <b.rappenecker@sutdent.isartdigital.com>
2020-10-18 13:14:30 +0200

    Son

---
+++ SonoBulle/Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static Music _instance;
+     
+    void Awake () {
+        if (!_instance) 
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+            Destroy(gameObject);
+    }
+}
